d73eaf8157e10455d48ccc245d2d65f27c0738ac
David Hurtado <david.hr97110105243@gmail.com>
2022-09-22 15:49:23 -0500

    2commit

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -5,7 +5,10 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'>
+      <h1>Los personajes favoritos del Anime:</h1>
       <Testimonio />
+      </div>
     </div>
   );
 }
