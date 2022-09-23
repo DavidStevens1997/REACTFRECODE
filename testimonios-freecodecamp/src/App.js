91a712b016e6c99898ff935ab59a67cbd2a86a2b
David Hurtado <david.hr97110105243@gmail.com>
2022-09-23 13:18:48 -0500

    1proyecto finish

--- testimonios-freecodecamp/src/App.js
+++ testimonios-freecodecamp/src/App.js
@@ -7,7 +7,27 @@ function App() {
     <div className="App">
       <div className='contenedor-principal'>
       <h1>Los personajes favoritos del Anime:</h1>
-      <Testimonio />
+      <Testimonio 
+      nombre='Monkey D. Luffy'
+      pais='Wano'
+      imagen='luffy-2'
+      cargo='Capitan'
+      empresa='los sombreros de paja'
+      testimonio='es el protagonista principal de la serie de manga y anime One Piece. Es el capitán y fundador de los Piratas de Sombrero de Paja así como un de los Cuatro Emperadores que gobiernan los mares del Nuevo Mundo.' />
+      <Testimonio
+      nombre='Gon Freecss'
+      pais='Greed Island'
+      imagen='gon'
+      cargo='Cazador'
+      empresa='Hunter x Hunter' 
+      testimonio='es el protagonista principal de la serie de manga y anime Hunter × Hunter. Está en búsqueda de su padre, el famoso cazador Ging Freecss.' />
+      <Testimonio 
+      nombre='Eren Yeager'
+      pais='Muralla María'
+      imagen='eren'
+      cargo='Titan'
+      empresa='Shingeki no Kyojin' 
+      testimonio=' Es el único hijo de Grisha y Carla Jaeger. A su vez, es el medio hermano menor de Zeke Jaeger, el hermano adoptivo de Mikasa Ackerman y un Titán Cambiante, siendo el último portador del Titán de Ataque.' />
       </div>
     </div>
   );
--- testimonios-freecodecamp/src/componentes/Testimonio.js
+++ testimonios-freecodecamp/src/componentes/Testimonio.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return(
     <div className='contenedor-testimonio'>
       <img className='imagen-testimonio'
-      src={require('../imagenes/luffy-2.png')}
-      alt='Foto de Luffy'/>
+      src={require(`../imagenes/${props.imagen}.png`)} 
+      alt='Foto de Personaje' />
       <div className='contenedor-texto-testimonio'>
-        <p className='nombre-testimonio'>Monkey D. Luffy en Wano</p>
-        <p className='cargo-testimonio'>Capitan de los sombreros de paja</p>
-        <p className='texto-testimonio'>Monkey D. Luffy es el protagonista principal de la serie de manga y anime One Piece. Es el capitán y fundador de los Piratas de Sombrero de Paja así como un de los Cuatro Emperadores que gobiernan los mares del Nuevo Mundo.</p>
+        <p className='nombre-testimonio'><strong>{props.nombre} en {props.pais}</strong></p>
+        <p className='cargo-testimonio'>{props.cargo} de <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>{props.testimonio}</p>
       </div>
     </div>
   );
